40fea152211c56527cc84d00620296de368bb5a1
Abedur Rahman <abedur@gmail.com>
2023-11-07 11:26:16 -0500

    Initial commit

---
+++ app/src/main/java/com/miu/table/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.miu.table
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TableLayout
+
+import android.widget.TableRow
+import android.widget.TextView
+import com.miu.table.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    private lateinit var tableLayout:TableLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(binding.root)
+
+
+        tableLayout = binding.tblLayout
+
+        binding.addBtn.setOnClickListener {
+            val version = binding.version.text.toString();
+            val code = binding.code.text.toString();
+
+            if(binding.version.text.trim().isNullOrEmpty()){
+                binding.version.error = "Version must be enter.";
+            }else if(binding.code.text.trim().isNullOrEmpty()){
+                binding.code.error = "Code must be entered."
+            }
+            else{
+                val tableRow = TableRow(this);
+                val versionTextView = TextView(this);
+                versionTextView.text = version;
+                val codeTextView = TextView(this);
+                codeTextView.text = code;
+
+                tableRow.addView(versionTextView);
+                tableRow.addView(codeTextView);
+                tableLayout.addView(tableRow);
+//                binding.version.text.clear();
+//                binding.code.text.clear();
+            }
+        }
+    }
+
+}
